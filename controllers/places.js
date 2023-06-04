bfaff02640447f68e05b947b0e6ac237ec993f2e
Rene A Larson <122948946+ReneLarson1990@users.noreply.github.com>
2023-06-03 19:31:11 -0700

    new place is not posting a new place making a commit to send to TA for help with issue

--- controllers/places.js
+++ controllers/places.js
@@ -1,10 +1,30 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
+
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+
+router.get('/places/:id', (req, res) => {
+  res.render('places/new')
+})
+
 
 router.post('/', (req, res) => {
   console.log(req.body)
-  res.send('POST /places')
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
-
 
 
 router.get('/', (req, res) => {
